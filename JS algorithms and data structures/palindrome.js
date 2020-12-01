f629e77e998e699403092e465c77c4eb9c0092cf
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2020-12-01 09:07:07 +0200

    Version 1.12.2020

--- JS algorithms and data structures/palindrome.js
+++ JS algorithms and data structures/palindrome.js
@@ -1,21 +1,21 @@
 function palindrome(str) {
-  var letters = []; 
+  var letters = [];
   for (var i = 0; i < str.length; i++) {
     if ((/[a-zA-Z0-9]/).test(str[i]) === true) {
-          letters.push(str[i].toLowerCase());
+      letters.push(str[i].toLowerCase());
     }
-   }
-   
-   let j = letters.length;
-    while (j >= 1) {
-      if (letters[0] === letters[letters.length - 1]) {
-        letters = letters.slice(1, letters.length - 1);
-        j -= 2;  
-      } else {
-          return false;
-      } 
+  }
+
+  let j = letters.length;
+  while (j >= 1) {
+    if (letters[0] === letters[letters.length - 1]) {
+      letters = letters.slice(1, letters.length - 1);
+      j -= 2;
+    } else {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 console.log(palindrome("A man, a plan, a canal. Panama"));
