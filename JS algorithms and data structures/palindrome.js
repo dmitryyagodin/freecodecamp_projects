992ef9908efdf6ba499fc5e2f9d002e5fc262b6e
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2020-12-01 09:05:18 +0200

    Palindrome.js cleaner

--- JS algorithms and data structures/palindrome.js
+++ JS algorithms and data structures/palindrome.js
@@ -1,13 +1,12 @@
 function palindrome(str) {
   var letters = []; 
   for (var i = 0; i < str.length; i++) {
-    if ((/[a-zA-Z]/).test(str[i]) === true) {
-          letters.push(str[i]);
+    if ((/[a-zA-Z0-9]/).test(str[i]) === true) {
+          letters.push(str[i].toLowerCase());
     }
    }
    
-  function sameLetter(letters) {
-    let j = letters.length;
+   let j = letters.length;
     while (j >= 1) {
       if (letters[0] === letters[letters.length - 1]) {
         letters = letters.slice(1, letters.length - 1);
@@ -17,8 +16,6 @@ function palindrome(str) {
       } 
     }
     return true;
-  }  
-  return sameLetter(letters);
 }
 
-console.log(palindrome("ey %e "));
+console.log(palindrome("A man, a plan, a canal. Panama"));
