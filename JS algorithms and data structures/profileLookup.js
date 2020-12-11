2b850ec6a48a9cab549e696e6a8fd081cf042bc5
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2020-12-11 11:34:57 +0200

    Initial solution

---
+++ JS algorithms and data structures/profileLookup.js
@@ -0,0 +1,49 @@
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i]["firstName"] === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+            return "No such property";
+        }
+    }
+  }
+  return "No such contact";
+}
+
+// console.log(lookUpProfile("Akira", "likes")); // =>
+console.log(lookUpProfile("Kristian", "lastName")); // => "Vos"
+console.log(lookUpProfile("Sherlock", "likes")); // => ["Intriguing Cases", "Violin"]
+// console.log(lookUpProfile("Harry", "likes")); // => array
+// console.log(lookUpProfile("Bob", "number")); // => "No such contact"
+// console.log(lookUpProfile("Bob", "potato")); // => "No such contact"
+// console.log(lookUpProfile("Akira", "address")); // => "No such property"
