651ac3552ca642d19ab977394dc45101f1aa5625
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2021-02-01 14:52:58 +0200

    Render TypesOfFood

---
+++ Front End Libraries/React/my-fcc-app/src/TypesOfFood.js
@@ -0,0 +1,14 @@
+import Fruits from './Fruits';
+import Vegetables from './Vegetables';
+
+function Types_of_Food() {
+  return (
+    <div>
+      <h1>Types of Food:</h1>
+      <Fruits />
+      <Vegetables />
+    </div>
+  );
+}
+
+export default Types_of_Food;
---
+++ Front End Libraries/React/my-fcc-app/src/Vegetables.js
@@ -0,0 +1,14 @@
+function Vegetables() {
+  return (
+    <div>
+      <p>Vegetables</p>
+      <ul>
+        <li>Potato</li>
+        <li>Tomato</li>
+        <li>Onion</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Vegetables;
